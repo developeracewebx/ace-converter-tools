fcb0ad7ff1bae28a5cbaf7f37d048431d09cd62d
AceWebX <developer@acewebx.com>
2024-03-05 13:51:44 +0530

    version update

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: acewebx
 Tags: Color Converter, Energy Converter, Speed Converter, Pressure Converter, DMS Converter
 Requires at least: 3.0.1
 Tested up to: 6.4.2
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Donate link: https://www.paypal.me/acewebx
--- ace-converter-tools.php
+++ ace-converter-tools.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Ace Converter Tools
  * Plugin URI:        https://wordpress.org/plugins/ace-converter-tools
  * Description:       This plugin contain multiple converter tools, which can be access via simple shortcodes.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            AceWebx Team
  * Author URI:        https://acewebx.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ACE_CONVERTER_TOOLS_VERSION', '1.0.0' );
+define( 'ACE_CONVERTER_TOOLS_VERSION', '1.0.1' );
 define( 'ACE_CONVERTER_TOOLS_URL', plugin_dir_url( __FILE__ ) );
 define( 'ACE_CONVERTER_TOOLS_PATH', plugin_dir_path( __FILE__ ) );
 
